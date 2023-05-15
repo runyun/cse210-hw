644a361b310b920e6332a0abb8ac261b5a2026ef
runyun <runningyun@gmail.com>
2023-05-15 22:42:44 +0800

    Finished Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("white", 5);
+
+        Rectangle rectangle = new Rectangle("blue", 7, 2);
+
+        Circle circle = new Circle("yellow", 4);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine();
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
+        
     }
 }
---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,15 @@
+public class Rectangle : Shape
+{
+    private double _length, _width;
+
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
